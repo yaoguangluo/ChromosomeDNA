9b527729618a423cd213d212cfa5015cbf736890
浏阳德塔软件开发有限公司GPL2.0开源大数据项目集 <2080315360@qq.com>
2022-02-02 15:12:18 +0800

    乱码修正
    罗瑶光

--- 2022/02/02/StaticReflectionMapM_IDUQ_C.java
+++ 2022/02/02/StaticReflectionMapM_IDUQ_C.java
@@ -7,51 +7,51 @@ import java.util.Map;
 import SEM.bloom.StaticFunctionMap;
 import SEM.bloom.StaticFunctionMapM_IDUQ_E;
 
-//Öø×÷È¨ÈË+ ×÷Õß= ÂŞÑş¹â
+//è‘—ä½œæƒäºº+ ä½œè€…= ç½—ç‘¶å…‰
 public class StaticReflectionMapM_IDUQ_C {
 	public static Map<String, String> annotationMap= new HashMap<>();
 	public static void callFunction(String callFunctionKey
 			, StaticFunctionMapM_IDUQ_E staticFunctionMapM_IDUQ_C
 			, Map<String, Object> output) throws IOException {
-		String[] ´«²ÎÒò×Ó= (String[]) output.get("´«²ÎÒò×Ó");
+		String[] ä¼ å‚å› å­= (String[]) output.get("ä¼ å‚å› å­");
 		Object map = null;
 		if(callFunctionKey.equalsIgnoreCase("getQ_OulerRing")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getQ_OulerRing();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getQ_OulerRing16")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getQ_OulerRing16();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getDecadeToPDS")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getDecadeToPDS();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getDecadeToPDS16")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getDecadeToPDS16();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getPDE_PDS_DL")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getPDE_PDS_DL();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getPDE_PDS_DL16")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapM_IDUQ_C.getPDE_PDS_DL16();
 			}
