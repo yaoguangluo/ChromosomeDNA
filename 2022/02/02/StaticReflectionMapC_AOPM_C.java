57151f798a15f9413eedc7392bbca8984068361b
浏阳德塔软件开发有限公司GPL2.0开源大数据项目集 <2080315360@qq.com>
2022-02-02 15:04:36 +0800

    乱码纠正
    罗瑶光

--- 2022/02/02/StaticReflectionMapC_AOPM_C.java
+++ 2022/02/02/StaticReflectionMapC_AOPM_C.java
@@ -13,204 +13,204 @@ import OSI.AOP.MS.VPC.server.VPCSResponse;
 import SEM.bloom.StaticFunctionMap;
 import SEM.bloom.StaticFunctionMapC_AOPM_E;
 
-//Öø×÷È¨ÈË+ ×÷Õß= ÂŞÑş¹â
+//è‘—ä½œæƒäºº+ ä½œè€…= ç½—ç‘¶å…‰
 public class StaticReflectionMapC_AOPM_C {
 	public static Map<String, String> annotationMap= new HashMap<>();
 	@SuppressWarnings("unchecked")
 	public static void callFunction(String callFunctionKey
 			, StaticFunctionMapC_AOPM_E staticFunctionMapC_AOPM_C, Map<String, Object> output) throws Exception {
-		String[] ´«²ÎÒò×Ó= (String[]) output.get("´«²ÎÒò×Ó");
-		int Òò×Ó= 0;
+		String[] ä¼ å‚å› å­= (String[]) output.get("ä¼ å‚å› å­");
+		int å› å­= 0;
 		Object map = null;
 		if(callFunctionKey.equalsIgnoreCase("BootVPCSBackEnd")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.BootVPCSBackEnd((App)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.BootVPCSBackEnd((App)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("requestIpFilter")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.requestIpFilter((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.requestIpFilter((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("requestLinkFilter")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.requestLinkFilter((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.requestLinkFilter((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("requestIpFix")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.requestIpFix((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.requestIpFix((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("requestLinkFix")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.requestLinkFix((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.requestLinkFix((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("IV_BlockList")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				staticFunctionMapC_AOPM_C.IV_BlockList();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("requestLinkRecoder")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.requestLinkRecoder((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]), (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.requestLinkRecoder((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++]), (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("IQ_ForwardType")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.IQ_ForwardType((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.IQ_ForwardType((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("forwardToRestMap")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.forwardToRestMap((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.forwardToRestMap((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("IV_ServerInit_C_VPCSFrontEnd")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
 				staticFunctionMapC_AOPM_C.IV_ServerInit_C_VPCSFrontEnd();
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("IV_Server")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.IV_Server((App)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.IV_Server((App)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("IV_Service")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.IV_Service((JTextPane)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (String)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.IV_Service((JTextPane)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (String)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("IV_ServerServerInit_C")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.IV_ServerServerInit_C((App)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.IV_ServerServerInit_C((App)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("IQ_Response")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.IQ_Response((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.IQ_Response((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("returnResponse")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.returnResponse((Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.returnResponse((Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("P_Rest")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_Rest((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_Rest((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("P_View")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_View((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_View((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("P_Bytes")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_Bytes((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_Bytes((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("P_Buffer")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_Buffer((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_Buffer((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("P_BufferBytes")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_BufferBytes((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_BufferBytes((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("P_BytesWithoutZip")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.P_BytesWithoutZip((VPCSRequest)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (VPCSResponse)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.P_BytesWithoutZip((VPCSRequest)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (VPCSResponse)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("hugPillow")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				staticFunctionMapC_AOPM_C.hugPillow((Sleeper_H)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (Socket)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]), (int)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				staticFunctionMapC_AOPM_C.hugPillow((Sleeper_H)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (Socket)inputValues.get(ä¼ å‚å› å­[å› å­++]), (int)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 
 		if(callFunctionKey.equalsIgnoreCase("forward")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapC_AOPM_C.forward((String)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++])
-						, (Map<String, String>)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				map= staticFunctionMapC_AOPM_C.forward((String)inputValues.get(ä¼ å‚å› å­[å› å­++])
+						, (Map<String, String>)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getFilePath")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapC_AOPM_C.getFilePath((String)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				map= staticFunctionMapC_AOPM_C.getFilePath((String)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("getCode")){ 
-			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ´«²ÎÒò×Ó);
+			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, ä¼ å‚å› å­);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapC_AOPM_C.getCode((String)inputValues.get(´«²ÎÒò×Ó[Òò×Ó++]));
+				map= staticFunctionMapC_AOPM_C.getCode((String)inputValues.get(ä¼ å‚å› å­[å› å­++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
